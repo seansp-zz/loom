7ffcad42d55295f04fb4fc433dc5cf322aeab503
Sean Spratt <seansp@microsoft.com>
2017-12-05 23:34:05 +0000

    Added timestamper and credentials-binding

--- initJenkinsWorker/sleep_install_enslave.sh
+++ initJenkinsWorker/sleep_install_enslave.sh
@@ -2,7 +2,9 @@
 sleep 10
 wget http://jenkins:8080/jnlpJars/jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin startup-trigger-plugin
-java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin powershell
+java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin plain-credentials
+java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin timestamper
+java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin credentials-binding
 java -jar jenkins-cli.jar -s http://jenkins:8080/ install-plugin swarm
 java -jar jenkins-cli.jar -s http://jenkins:8080/ restart
 
